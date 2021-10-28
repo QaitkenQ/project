b4afdccf4f095dcc4c8f52844b5c4554d2adfb16
Kos <prowolf@bk.ru>
2021-10-28 12:59:08 +0300

    firs commit

---
+++ script_edu.js
@@ -0,0 +1,32 @@
+
+
+let persone = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp']
+console.log (arr[0]);
+
+/*let answer = confirm ("Are you shure");
+console.log(answer);*/
+
+//let answer = prompt ("Do you have 18?", "Yes");
+//console.log(answer);
+
+let incr = 10,
+    decr = 10;
+
+incr--;
+decr++;
+    
+console.log(incr);
+console.log(decr);
+
+let isChecked = true;
+    isClose = false;
+
+console.log(isChecked && isClose);
